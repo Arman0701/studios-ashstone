0d9f4656b29fa40e7aa0e33373f2f9fd633cad16
Arman Tadevosyan <tadevosyan889@gmail.com>
2024-05-16 01:02:26 +0400

    Project initialized.

---
+++ src/firebase/config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: import.meta.env.F_API_KEY,
+    authDomain: import.meta.env.F_AUTH_DOMAIN,
+    projectId: import.meta.env.F_PROJECT_ID,
+    storageBucket: import.meta.env.F_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.F_MESSAGING_SENDER_ID,
+    appId: import.meta.env.F_APP_ID
+};
